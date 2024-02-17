60529a06436fd67cf3e5a939a14665233faefdf5
Joel D <joeldasari10@gmail.com>
2024-02-17 19:22:07 +0530

    created routes and added a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="h-[10vh] w-full bg-slate-200 flex justify-between items-center px-[100px]">
+      <h1 className="text-3xl font-semibold">MERN Authentication</h1>
+      <div className="flex gap-8 text-lg font-medium">
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/about">
+          <p>About</p>
+        </Link>
+        <Link to="/profile">
+          <p>Profile</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
